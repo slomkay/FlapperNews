28a9bda475b06ce7d4cbfe2216ee85cc1b4de67a
slomkay <slomkay@gmail.com>
2016-03-03 10:40:29 +0200

    add post factory

--- app.js
+++ app.js
@@ -1,17 +1,16 @@
 var app = angular.module('flapperNews', []);
 
+app.factory('posts', [function(){
+    return {
+        posts: []
+    };
+}]);
+
 app.controller('MainCtrl', [
     '$scope',
-    function ($scope) {
-        $scope.test = 'hello world';
-
-        $scope.posts = [
-            {title: 'post 1', upvotes: 5},
-            {title: 'post 2', upvotes: 2},
-            {title: 'post 3', upvotes: 15},
-            {title: 'post 4', upvotes: 9},
-            {title: 'post 5', upvotes: 4}
-        ];
+    'posts',
+    function ($scope, posts) {
+        $scope.posts = posts.posts;
 
         $scope.addPost = function(){
             if(!$scope.title || $scope.title === '') {
